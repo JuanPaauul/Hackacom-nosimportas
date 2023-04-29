fff6d854cb9ba5b932dbb74af90feace2de9ee93
JuanPCarrasco <jp.carrasco.tx@gmail.com>
2023-04-29 09:55:30 -0400

    Bottom navigations view implemented

--- app/src/main/java/com/moresoft/nosimportashackacom/MainActivity.kt
+++ app/src/main/java/com/moresoft/nosimportashackacom/MainActivity.kt
@@ -2,10 +2,43 @@ package com.moresoft.nosimportashackacom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.moresoft.nosimportas.ConfidenceUsersFragment
+import com.moresoft.nosimportas.PanicButtonFragment
+import com.moresoft.nosimportas.ZonedMapFragment
+import com.moresoft.nosimportashackacom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(ZonedMapFragment())
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.confidence_users -> {
+                    replaceFragment(ConfidenceUsersFragment())
+                }
+                R.id.panic_button -> {
+                    replaceFragment(PanicButtonFragment())
+                }
+                R.id.zoned_map -> {
+                    replaceFragment(ZonedMapFragment())
+                }
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
