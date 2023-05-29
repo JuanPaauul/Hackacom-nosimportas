84c7ced143bb906f7ee3442688a76c657b922d7b
JuanPCarrasco <jp.carrasco.tx@gmail.com>
2023-05-29 00:43:39 -0400

    Showing confidence users from API
    	#	modified:   usercases/src/main/java/com/moresoft/usercases/EndPointApi.kt

--- app/src/main/java/com/moresoft/nosimportashackacom/UserListAdapter.kt
+++ app/src/main/java/com/moresoft/nosimportashackacom/UserListAdapter.kt
@@ -13,7 +13,7 @@ import com.moresoft.domain.ConfidenceUser
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
 
-class UserListAdapter(private val items: ArrayList<ConfidenceUser>, val context: ConfidenceUsersFragment): RecyclerView.Adapter<UserListAdapter.UserListViewHolder>(){
+class UserListAdapter(private var items: List<ConfidenceUser>, val context: ConfidenceUsersFragment): RecyclerView.Adapter<UserListAdapter.UserListViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.user_confidence, parent, false)
         return UserListViewHolder(v)
@@ -23,20 +23,21 @@ class UserListAdapter(private val items: ArrayList<ConfidenceUser>, val context:
     }
     override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
         val item = items[position]
-        val nameTextView = holder.itemView.findViewById<TextView>(R.id.tv_confidence_name)
-        nameTextView?.text = item.name
-        holder.itemView.findViewById<TextView>(R.id.tv_confidence_lastname)?.text = item.lastName
+        holder.itemView.findViewById<TextView>(R.id.tv_confidence_name)?.text = item.first_name
+        holder.itemView.findViewById<TextView>(R.id.tv_confidence_lastname)?.text = item.last_name
         holder.itemView.findViewById<TextView>(R.id.tv_confidence_email)?.text = item.email
 
         val imageView = holder.itemView.findViewById<ImageView>(R.id.iv_confidence_image)
-
         if (imageView != null) {
             Picasso.get()
-                .load("https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")
+                .load(item.avatar)
                 .error(R.mipmap.ic_launcher_round)
                 .into(imageView)
         }
-
+    }
+    fun updateData(users: List<ConfidenceUser>) {
+        items = users
+        notifyDataSetChanged()
     }
     class UserListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 }
--- usercases/src/main/java/com/moresoft/usercases/EndPointApi.kt
+++ usercases/src/main/java/com/moresoft/usercases/EndPointApi.kt
@@ -2,10 +2,14 @@ package com.moresoft.usercases
 
 import com.moresoft.domain.ConfidenceUser
 import com.moresoft.domain.ConstantsRestApi
+import com.moresoft.domain.UsersApiResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 interface EndPointApi {
     @GET(ConstantsRestApi.POSTS)
-    fun getAllPost(): Call<List<ConfidenceUser>>
+    fun getAllPost(): Call<UsersApiResponse>
+    //fun getAllPost(): Call<List<ConfidenceUser>>
 }
